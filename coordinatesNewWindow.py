b9889aa0b6eeb22111273ca9cdc5afde3194e85d
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-11 23:14:23 +0530

    fill another window with clicked color

---
+++ coordinatesNewWindow.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+#events = [i for i in dir(cv2) if 'EVENT' in i]
+#dir shows all classes and members inside cv2
+#print(events)
+
+def click_event(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        blue = img[y, x, 0]  # take the blue value for a point
+        green = img[y, x, 1]
+        red = img[y, x, 2]
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+        colorImg = np.zeros((512, 512, 3), np.uint8)
+        colorImg[:] = [blue, green, red]
+        cv2.imshow('color',colorImg)
+
+
+#img = np.zeros((512,512,3), np.uint8)
+img = cv2.imread('lena.jpg')
+cv2.imshow('image', img)
+points = []
+
+cv2.setMouseCallback('image', click_event)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
