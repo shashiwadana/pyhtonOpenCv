379086e7312195eb6b05bd2217e99e871f2c9cb6
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-12 09:41:42 +0530

    bitwise not changed for images added

--- bitwise.py
+++ bitwise.py
@@ -8,7 +8,7 @@ img2 = cv2.imread('lena.jpg')
 #bitAnd = cv2.bitwise_and(img1, img2)
 #bitOr = cv2.bitwise_or(img1,img2)
 #bitxOr = cv2.bitwise_xor(img1,img2)
-bitnot = cv2.bitwise_not(img1,img2)
+bitnot = cv2.bitwise_not(img2)
 
 
 #cv2.imshow('image',img1)
