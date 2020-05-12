455520c222a962d8f9db37d13764c3b2aecaff40
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-12 10:09:12 +0530

    adding tracked values with switch for images added

--- trackBar.py
+++ trackBar.py
@@ -11,6 +11,9 @@ cv2.createTrackbar('B','image', 0, 255, nothing)
 cv2.createTrackbar('G','image', 0, 255, nothing)
 cv2.createTrackbar('R','image', 0, 255, nothing)
 
+swich = '0:OFF\n 1:ON'
+cv2.createTrackbar('switch','image', 0, 1, nothing)
+
 while(1):
     cv2.imshow('image',img)
     k = cv2.waitKey(1) & 0xFF
@@ -20,6 +23,11 @@ while(1):
     b=cv2.getTrackbarPos('B','image')
     g = cv2.getTrackbarPos('G', 'image')
     r = cv2.getTrackbarPos('R', 'image')
-    img[:] = [b,g,r]
+    s = cv2.getTrackbarPos('switch', 'image')
+
+    if s == 0:
+        img[:] = 0
+    else:
+        img[:] = [b,g,r]
 
 cv2.destroyAllWindows()
