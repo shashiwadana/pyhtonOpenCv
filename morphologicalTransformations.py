ff52eddf82a46157ebfe5911d86ef79edd1eb7a7
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-14 16:10:47 +0530

    opening added

--- morphologicalTransformations.py
+++ morphologicalTransformations.py
@@ -8,12 +8,14 @@ _,mask = cv2.threshold(img,220,255,cv2.THRESH_BINARY_INV)
 kernel = np.ones((2,2),np.uint8)
 dilation = cv2.dilate(mask,kernel,iterations=2)
 erosion = cv2.erode(mask,kernel,iterations=1) #erode the boundary of foreground objects
+opening =cv2.morphologyEx(mask, cv2.MORPH_OPEN,kernel)#erosion+dialation
 
-titles = ['image','mask','dialation','erosion']
-images = [img,mask,dilation,erosion]
 
-for i in range(4):
-    plt.subplot( 2, 2, i+1),plt.imshow(images[i],'gray')
+titles = ['image','mask','dialation','erosion','opening','closing']
+images = [img,mask,dilation,erosion,opening,closing]
+
+for i in range(6):
+    plt.subplot( 2, 3, i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 plt.show()
