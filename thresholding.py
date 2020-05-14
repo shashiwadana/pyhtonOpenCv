d52c34fc05c005b65766ef651c78c548f2c79fdc
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-14 10:28:13 +0530

    threshold trunc added added

--- thresholding.py
+++ thresholding.py
@@ -4,10 +4,16 @@ import cv2
 img = cv2.imread('gradient.png', 0)
 _, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) #compare with thresh and assign 0 or 1
 _, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+_, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC) #upto the threshold values of pixels won't be changed,after thresh pixel value remains same
+#_, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+#_, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
 
 cv2.imshow('image',img)
 cv2.imshow('threshold 1',th1)
 cv2.imshow('threshold 2',th2)
+cv2.imshow('threshold 3',th3)
+#cv2.imshow('threshold 2',th2)
+#cv2.imshow('threshold 2',th2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
