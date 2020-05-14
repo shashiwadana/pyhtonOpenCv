5219abb632eb96778856b4289b77f2d61b70b080
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-14 15:12:25 +0530

    erosion added

--- morphologicalTransformations.py
+++ morphologicalTransformations.py
@@ -5,14 +5,15 @@ from matplotlib import pyplot as plt
 img = cv2.imread('smarties.png', cv2.IMREAD_GRAYSCALE)
 _,mask = cv2.threshold(img,220,255,cv2.THRESH_BINARY_INV)
 
-kernel = np.ones((5,5),np.uint8)
+kernel = np.ones((2,2),np.uint8)
 dilation = cv2.dilate(mask,kernel,iterations=2)
+erosion = cv2.erode(mask,kernel,iterations=1) #erode the boundary of foreground objects
 
-titles = ['image','mask','dialation']
-images = [img,mask,dilation]
+titles = ['image','mask','dialation','erosion']
+images = [img,mask,dilation,erosion]
 
-for i in range(3):
-    plt.subplot( 1, 3, i+1),plt.imshow(images[i],'gray')
+for i in range(4):
+    plt.subplot( 2, 2, i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 plt.show()
