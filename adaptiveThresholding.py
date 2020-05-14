9c4764b9a2d7f6a1d99c5cd892593c1f49d8b8e8
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-14 10:59:55 +0530

    addaptive thresh gaussian added

--- adaptiveThresholding.py
+++ adaptiveThresholding.py
@@ -4,8 +4,9 @@ import cv2
 img = cv2.imread('sudoku.png', 0)
 _, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 th2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2) #take the mean of neighbours
+th3 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
 cv2.imshow('image',img)
-cv2.imshow('threshold 2',th2)
+cv2.imshow('threshold 2',th3)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
