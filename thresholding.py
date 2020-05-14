d6e0faaf661a1d3f08146a01ae073fbcba63716e
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-14 10:33:46 +0530

    threshold toZero inverse added added

--- thresholding.py
+++ thresholding.py
@@ -6,14 +6,14 @@ _, th1 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) #compare with thresh an
 _, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
 _, th3 = cv2.threshold(img, 127, 255, cv2.THRESH_TRUNC) #upto the threshold values of pixels won't be changed,after thresh pixel value remains same
 _, th4 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO)#pixel value<thresh assign it to 0
-#_, th2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
+_, th5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 
 cv2.imshow('image',img)
 #cv2.imshow('threshold 1',th1)
 #cv2.imshow('threshold 2',th2)
 #cv2.imshow('threshold 3',th3)
-cv2.imshow('threshold 2',th4)
-#cv2.imshow('threshold 2',th2)
+cv2.imshow('threshold 4',th4)
+cv2.imshow('threshold 5',th5)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
