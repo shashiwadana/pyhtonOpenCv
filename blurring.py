fc48e66f0d896342d4412b4bd98e50463a798653
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-16 23:34:09 +0530

    gaussian blur added

--- blurring.py
+++ blurring.py
@@ -8,11 +8,14 @@ img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
 kernel = np.ones((5,5),np.float32)/25 #homogeneous filtering
 dst = cv2.filter2D(img,-1,kernel)
 blur = cv2.blur(img,(5,5))
-titles = ['image','2D','blur']
-images = [img,dst,blur]
+gblur = cv2.GaussianBlur(img,(5,5),0) #gaussian blur is to remove high frequency noise
 
-for i in range(3):
-    plt.subplot( 1, 3, i+1),plt.imshow(images[i],'gray')
+
+titles = ['image','2D','blur','gblur']
+images = [img,dst,blur,gblur]
+
+for i in range(4):
+    plt.subplot( 2, 2, i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 plt.show()
