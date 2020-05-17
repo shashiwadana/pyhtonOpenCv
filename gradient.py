1a13f17fff9a1f3dacaeaa8138cbd7d27f53f292
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-17 14:25:00 +0530

    sobel combined edge detection added

--- gradient.py
+++ gradient.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('messi5.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('sudoku.png',cv2.IMREAD_GRAYSCALE)
 lap = cv2.Laplacian(img,cv2.CV_64F,ksize=3)
 lap = np.uint8(np.absolute(lap)) #get absolute value and transform to unsigned 8bit int
 sobleX= cv2.Sobel(img,cv2.CV_64F,1,0) #order od derivative x
@@ -10,11 +10,13 @@ sobleY= cv2.Sobel(img,cv2.CV_64F,0,1)
 
 sobleX =np.uint8(np.absolute(sobleX))
 sobleY =np.uint8(np.absolute(sobleY))
-titles = ['image','lap','sobX','sobY']
-images = [img,lap,sobleX,sobleY]
+sobelCombined=cv2.bitwise_or(sobleX,sobleY)
 
-for i in range(4):
-    plt.subplot( 2, 2, i+1),plt.imshow(images[i],'gray')
+titles = ['image','lap','sobX','sobY','soCom']
+images = [img,lap,sobleX,sobleY,sobelCombined]
+
+for i in range(5):
+    plt.subplot( 2, 3, i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 plt.show()
