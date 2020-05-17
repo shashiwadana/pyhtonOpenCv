03cac0c033927904f2da37566e363bfb22a4e2b9
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-17 14:19:42 +0530

    sobelX sobelY edge detection added

--- cannyEdge.py
+++ cannyEdge.py
@@ -5,13 +5,16 @@ from matplotlib import pyplot as plt
 img = cv2.imread('messi5.jpg',cv2.IMREAD_GRAYSCALE)
 lap = cv2.Laplacian(img,cv2.CV_64F,ksize=3)
 lap = np.uint8(np.absolute(lap)) #get absolute value and transform to unsigned 8bit int
+sobleX= cv2.Sobel(img,cv2.CV_64F,1,0) #order od derivative x
+sobleY= cv2.Sobel(img,cv2.CV_64F,0,1)
 
+sobleX =np.uint8(np.absolute(sobleX))
+sobleY =np.uint8(np.absolute(sobleY))
+titles = ['image','lap','sobX','sobY']
+images = [img,lap,sobleX,sobleY]
 
-titles = ['image','lap']
-images = [img,lap]
-
-for i in range(2):
-    plt.subplot( 1, 2, i+1),plt.imshow(images[i],'gray')
+for i in range(4):
+    plt.subplot( 2, 2, i+1),plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]), plt.yticks([])
 plt.show()
