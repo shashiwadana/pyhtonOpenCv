3800b177615e9ad1ede6d5041f6dda2ad04c24aa
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-17 15:11:40 +0530

    laplacian pyramids added

--- imagePyramid.py
+++ imagePyramid.py
@@ -7,7 +7,16 @@ gp =[layer]
 for i in range(6):
     layer=cv2.pyrDown(layer)
     gp.append(layer)
-    cv2.imshow(str(i),layer)
+    #cv2.imshow(str(i),layer)
+
+layer = gp[5]
+cv2.imshow('upper layer image',layer)
+lp = [layer]
+for i in range(5,0,-1):
+    gaussian_extend=cv2.pyrUp(gp[i])
+    laplacian = cv2.subtract(gp[i-1],gaussian_extend)
+    cv2.imshow(str(i),laplacian)
+
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
