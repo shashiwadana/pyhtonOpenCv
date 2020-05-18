b465bef7ccf90168b27675c2216d85a5cdcd5915
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-18 20:57:44 +0530

    contours drawing added

--- drawContors.py
+++ drawContors.py
@@ -6,6 +6,10 @@ imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 ret, thresh =cv2.threshold(imgray,127,255,0)
 contours, hierarchy =cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 print("No of contours = "+str(len(contours)))
+print(contours[0])
+cv2.drawContours(img,contours,-1,(0,255,0),3)
+
+
 cv2.imshow('image',img)
 cv2.imshow('gray image',imgray)
 cv2.waitKey(0)
