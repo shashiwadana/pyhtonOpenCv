05ead6bd33a3e38dc6aa1bdf09d75961217eb311
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-18 20:51:16 +0530

    contours count added

---
+++ drawContors.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+
+img = cv2.imread('opencv-logo.png')
+imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret, thresh =cv2.threshold(imgray,127,255,0)
+contours, hierarchy =cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+print("No of contours = "+str(len(contours)))
+cv2.imshow('image',img)
+cv2.imshow('gray image',imgray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
