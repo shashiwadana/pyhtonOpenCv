7f8c95693f01e82eaa18bc03e150eca06c3d3bad
zbuddy <shashiwadananirmani14@gmail.com>
2020-05-19 23:56:09 +0530

    shapes final added

--- shapeDetection.py
+++ shapeDetection.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('opencv-logo.png')
+img = cv2.imread('shapes.png')
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 _, thresh =cv2.threshold(imgray,240,255,cv2.THRESH_BINARY)
 contours, _ =cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
@@ -10,7 +10,7 @@ for contour in contours:
     approx = cv2.approxPolyDP(contour,0.01*cv2.arcLength(contour,True),True) #arcLength calculates contuor line length
     cv2.drawContours(img,[approx],0,(0,0,0),5)
     x = approx.ravel()[0]
-    y = approx.ravel()[1]
+    y = approx.ravel()[1]-5
     if len(approx) == 3:
         cv2.putText(img,"Triangle",(x,y),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0))
     elif len(approx) == 4:
